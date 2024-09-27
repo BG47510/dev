3d0299866389e8985c105e14cc173008bef36f30
Zern <75311133+BG47510@users.noreply.github.com>
2024-09-27 13:37:55 +0200

    Update api_key_test.py

--- api_key_test.py
+++ api_key_test.py
@@ -51,7 +51,8 @@ programs = []
 # Convertie la chaîne JSON en dictionnaire Python.
 data = reponse.json() # La mise à jour de télérama est à 06h00.
 # if reponse.status_code == 200:
-programs += data.get('donnees') # donne une liste
+programs += data.get('donnees'[]) # donne une liste
+sett = dict()
 # break
 # else:
 # data = reponse.json()
@@ -68,6 +69,7 @@ programs += data.get('donnees') # donne une liste
 # print(type(data)) # > class 'dict'
 
 print(programs)
+print(sett)
 # *******************************************************************************************
 
 # def tvg_id(args):
