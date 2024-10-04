379712fd58e7662f8c02b1d837455a926069cc44
Zern <75311133+BG47510@users.noreply.github.com>
2024-10-04 13:24:46 +0200

    Update api_key_test.py

--- api_key_test.py
+++ api_key_test.py
@@ -71,7 +71,8 @@ for prog in data:
 print(program_)
 #  'horaire': {'debut': '2024-09-13 06:00:00', 'fin': '2024-09-13 06:30:00'}
 # start_time_mili = int(data['horaire']['debut']['startTime']['value']) / 1000
-titre = data.get('titre')
+titre = data[2]
+#titre = data.get('titre')
 print(titre)
 for slot in reponse.json()['donnees'][0]:
     epg_dict = {}
