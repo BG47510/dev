cb38f10b41c2f241071462371b92355c777d25e3
Zern <75311133+BG47510@users.noreply.github.com>
2024-10-10 16:59:03 +0200

    Update api_key_test.py

--- api_key_test.py
+++ api_key_test.py
@@ -156,14 +156,7 @@ print(program_dict)
 
 # pour lire et comprendre les données
 #tree = ET.parse(data)
-now = datetime.datetime.now()
-print(now)
-next_day = now + timedelta(days=i)
-back_day = (now + timedelta(days=i)) - timedelta(days=1)
-date_to = next_day.strftime("%Y-%m-%d")
-date_from = back_day.strftime("%Y-%m-%d")
-date_ = next_day.strftime("%d.%m.%Y")
-print(date_, end="", flush=True)
+
 #programs += data.get('donnees', []) # donne une liste
 #output = ""
 #for key, value in data.items():
