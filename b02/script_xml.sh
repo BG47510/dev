ed8391d6b5e0f36be950211a88d126e5cccb65d6
Zern <75311133+BG47510@users.noreply.github.com>
2026-02-16 15:41:13 +0100

    Update script_xml.sh

--- b02/script_xml.sh
+++ b02/script_xml.sh
@@ -11,7 +11,6 @@ OUTPUT_FILE="filtered_epg.xml"
 
 # Créer le fichier de sortie avec l'en-tête XML
 echo '<?xml version="1.0" encoding="UTF-8"?>' > "$OUTPUT_FILE"
-echo '<!DOCTYPE tv SYSTEM "xmltv.dtd">' >> "$OUTPUT_FILE"
 echo '<tv>' >> "$OUTPUT_FILE"
 
 # Fonction pour extraire et filtrer le contenu
@@ -22,9 +21,9 @@ extract_and_filter() {
     # Télécharger et décompresser
     curl -s "$url" | gunzip > "$tmp_file"
 
-    # Pour chaque channel id, extraire les programmes
+    # Pour chaque channel id, extraire les chaînes et programmes
     for channel_id in "${CHANNEL_IDS[@]}"; do
-        xmlstarlet sel -t -m "/tv/channel[@id='$channel_id'] | /tv/programme[@channel='$channel_id']" -o "\n" "$tmp_file" >> "$OUTPUT_FILE"
+        xmlstarlet sel -n -t -m "/tv/channel[@id='$channel_id'] | /tv/programme[@channel='$channel_id']" -o "\n" "$tmp_file" >> "$OUTPUT_FILE"
     done
 
     rm "$tmp_file"
