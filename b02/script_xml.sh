36473e978373245dcc864297607b2ceb5281fa4f
Zern <75311133+BG47510@users.noreply.github.com>
2026-02-21 13:23:57 +0100

    Update script_xml.sh

--- b02/script_xml.sh
+++ b02/script_xml.sh
@@ -3,7 +3,7 @@
 # ==============================================================================
 # CONFIGURATION
 # ==============================================================================
-CHANNEL_IDS=("TF1.fr" "France2.fr" "MC174.api.telerama.fr")
+CHANNEL_IDS=("TF1.fr" "France2.fr" "C174.api.telerama.fr")
 
 URLS=(
     "https://xmltvfr.fr/xmltv/xmltv.xml.gz"
