7bf7275da090495ee57a8f7a741b58d407bfaf28
Zern <75311133+BG47510@users.noreply.github.com>
2026-02-21 11:39:58 +0100

    Update script_xml.sh

--- b02/script_xml.sh
+++ b02/script_xml.sh
@@ -9,7 +9,7 @@ URLS=("https://xmltvfr.fr/xmltv/xmltv.xml.gz")
 # Fichier de sortie
 OUTPUT_FILE="filtered_epg.xml"
 
-# Créer le fichier de sortie avec l'en-tête XML
+# Créer une nouvelle sortie
 echo '<?xml version="1.0" encoding="UTF-8"?>' > "$OUTPUT_FILE"
 echo '<tv>' >> "$OUTPUT_FILE"
 
@@ -32,22 +32,33 @@ extract_and_filter() {
         echo "Extraction pour le channel_id: $channel_id"
 
         # Extraction des chaînes
-        xmlstarlet sel -t -m "/tv/channel[@id='$channel_id']" \
+        channel_data=$(xmlstarlet sel -t -m "/tv/channel[@id='$channel_id']" \
             -o "<channel id='$channel_id'>\n" \
             -o "<display-name>" -v "display-name" -o "</display-name>\n" \
             -o "</channel>\n" \
-            "$tmp_file" >> "$OUTPUT_FILE"
+            "$tmp_file")
+
+        # Écrire les chaînes dans le fichier de sortie
+        if [[ -n "$channel_data" ]]; then
+            echo -e "$channel_data" >> "$OUTPUT_FILE"
+        fi
 
         # Extraction des programmes associés
-        xmlstarlet sel -t -m "/tv/programme[@channel='$channel_id']" \
+        programmes=$(xmlstarlet sel -t -m "/tv/programme[@channel='$channel_id']" \
             -o "<programme start='" -v "@start" -o "' stop='" -v "@stop" -o "' channel='$channel_id'>\n" \
             -o "<title lang='fr'>" -v "title" -o "</title>\n" \
             -o "<desc lang='fr'>" -v "desc" -o "</desc>\n" \
             -o "<date>" -v "date" -o "</date>\n" \
             -o "</programme>\n" \
-            "$tmp_file" >> "$OUTPUT_FILE"
+            "$tmp_file")
+
+        # Écrire les programmes dans le fichier de sortie
+        if [[ -n "$programmes" ]]; then
+            echo -e "$programmes" >> "$OUTPUT_FILE"
+        fi
     done
 
+    # Nettoyer le fichier temporaire
     rm "$tmp_file"
 }
 
